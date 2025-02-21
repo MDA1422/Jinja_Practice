cf8e12188dfc396894d4ae2b2a837166c304daf8
M. Daahir <71685940+MDA1422@users.noreply.github.com>
2025-02-21 23:26:20 +0000

    update

---
+++ main.py
@@ -0,0 +1,8 @@
+import jinja2
+
+
+users = [
+        {"name": "Alice", "role": "Admin"},
+            {"name": "Bob", "role": "Editor"},
+                {"name": "Charlie", "role": "Viewer"}
+                ]
